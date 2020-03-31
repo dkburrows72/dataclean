50c9a5b93cff6c476adaf216f300da481047f5a4
Dennis Burrows <dkburrows72@gmail.com>
2020-03-31 18:04:08 -0400

    formatting changes

--- dataclean/dataclean.py
+++ dataclean/dataclean.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
-def check_line(in_string, quote_char = '"'):
-    
+
+def check_line(in_string, quote_char='"'):
+    """returns the count of comma separated items in in_string
+    """
+
     # find start and end of quotes if they exist
     quote_start = in_string.find(quote_char)
     comma_sub = 0
     if quote_start > -1:
-        quote_end = in_string.find(quote_char,quote_start+1)
-        comma_sub = in_string[quote_start:quote_end].count(",")  # count the number of commas inside the quotes
-    
+        quote_end = in_string.find(quote_char, quote_start+1)
+        comma_sub = in_string[quote_start:quote_end].count(
+            ",")  # count the number of commas inside the quotes
+
     # count columns (# commas + 1) and subtract commas inside quotes
     return in_string.count(",") + 1 - comma_sub
 
 
-def find_bad_data(filename, sep = ',', verbose = 'No'):
+def find_bad_data(filename, sep=',', verbose='No'):
     """Scans a csv file and checks for consistent number of columns.  
     Returns a list of line numbers that are inconsistent
     """
@@ -21,20 +25,17 @@ def find_bad_data(filename, sep = ',', verbose = 'No'):
     bad_lines = []
     line_num = 1
 
-    with open(filename,"r") as fp:
+    with open(filename, "r") as fp:
         num_columns = len(fp.readline().split(sep=sep))
 
         for line in fp:
             line_num += 1
-            if check_line(line,'"') != num_columns:
+            if check_line(line, '"') != num_columns:
                 bad_lines.append(str(line_num)+": "+line)
 
-    
     if verbose == 'Yes':
         print(f"The proper number of columns is {num_columns}")
         print("Bad lines:")
-        print(*bad_lines,sep='\n')
-
+        print(*bad_lines, sep='\n')
 
     return bad_lines
-
--- test_dataclean.py
+++ test_dataclean.py
@@ -2,12 +2,14 @@ import pytest
 
 import dataclean.dataclean as dc
 
+
 def test_dataclean_with_comma_sep():
-    bad_data = ["4: matthew,burrows,158 Harmon dr, box 1292, 8603833549\n",]
+    bad_data = ["4: matthew,burrows,158 Harmon dr, box 1292, 8603833549\n", ]
 
     assert dc.find_bad_data("testdata.csv") == bad_data
+
 
 def test_check_line():
     good_data = "matthew,burrows,'158 Harmon dr, box 1292', 8603833549\n"
 
-    assert dc.check_line(good_data,"'") == 4
+    assert dc.check_line(good_data, "'") == 4
