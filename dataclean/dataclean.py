c4a8722d5eeab15899581cb146a14b3c6145efd1
Dennis Burrows <dkburrows72@gmail.com>
2020-04-01 16:56:55 -0400

    formatting

--- dataclean/dataclean.py
+++ dataclean/dataclean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def check_line(in_string, quote_char='"'):
+def check_line(in_string, quote_char='"', sep=","):
     """returns the count of comma separated items in in_string
     """
 
@@ -9,15 +9,15 @@ def check_line(in_string, quote_char='"'):
     quote_start = in_string.find(quote_char)
     comma_sub = 0
     if quote_start > -1:
-        quote_end = in_string.find(quote_char, quote_start+1)
-        comma_sub = in_string[quote_start:quote_end].count(
-            ",")  # count the number of commas inside the quotes
+        quote_end = in_string.find(quote_char, quote_start + 1)
+        # count the number of commas inside the quotes
+        comma_sub = in_string[quote_start:quote_end].count(sep)
 
     # count columns (# commas + 1) and subtract commas inside quotes
-    return in_string.count(",") + 1 - comma_sub
+    return in_string.count(sep) + 1 - comma_sub
 
 
-def find_bad_data(filename, sep=',', verbose='No'):
+def find_bad_data(filename, sep=",", verbose="No"):
     """Scans a csv file and checks for consistent number of columns.  
     Returns a list of line numbers that are inconsistent
     """
@@ -31,11 +31,11 @@ def find_bad_data(filename, sep=',', verbose='No'):
         for line in fp:
             line_num += 1
             if check_line(line, '"') != num_columns:
-                bad_lines.append(str(line_num)+": "+line)
+                bad_lines.append(str(line_num) + ": " + line)
 
-    if verbose == 'Yes':
+    if verbose == "Yes":
         print(f"The proper number of columns is {num_columns}")
         print("Bad lines:")
-        print(*bad_lines, sep='\n')
+        print(*bad_lines, sep="\n")
 
     return bad_lines
--- test_dataclean.py
+++ test_dataclean.py
@@ -4,7 +4,9 @@ import dataclean.dataclean as dc
 
 
 def test_dataclean_with_comma_sep():
-    bad_data = ["4: matthew,burrows,158 Harmon dr, box 1292, 8603833549\n", ]
+    bad_data = [
+        "4: matthew,burrows,158 Harmon dr, box 1292, 8603833549\n",
+    ]
 
     assert dc.find_bad_data("testdata.csv") == bad_data
 
@@ -13,3 +15,9 @@ def test_check_line():
     good_data = "matthew,burrows,'158 Harmon dr, box 1292', 8603833549\n"
 
     assert dc.check_line(good_data, "'") == 4
+
+
+def test_check_line_tab_sep():
+    good_data = "matthew\tburrows\t'158 Harmon dr\t box 1292'\t8603833549\n"
+
+    assert dc.check_line(good_data, quote_char="'", sep="\t") == 4
